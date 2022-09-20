6c5fc564783c028d4817264c36c6fca4b7df85f9
JLenseele <lenseele.julien@gmail.com>
2022-09-20 10:15:21 +0200

    ajout de fonctionnalité et DEF pour recupérer toutes les URL de sections depuis la page principal, puis extraire et ecrire les datas

--- script_scrapping.py
+++ script_scrapping.py
@@ -2,10 +2,15 @@ import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import csv
 
-#def is_long_string(string):
-#   return len(string) > 200
-def ecriture(datas, nom_fichier):
+def parse(url):
+	reponse = requests.get(url)
+	page = reponse.content
+	soup = BeautifulSoup(page, "html.parser")
+	return soup
 
+def ecriture(datas, nom_fichier_section):
+
+# definition de l'en-tete pour le fichier final
 	en_tete = ['product_page_url'
 				,'universal_product_code (upc)'
 				,'title'
@@ -16,26 +21,23 @@ def ecriture(datas, nom_fichier):
 				,'category'
 				,'review_rating'
 				,'image_url']
-	
-	with open(nom_fichier, 'w', encoding="utf-8") as fichier_csv:
+# ecriture dans un fichier avec le nom de la section en cour + encoder en utf-8 pour eviter les erreurs de 'charmap'
+	with open(nom_fichier_section, 'w', encoding="utf-8") as fichier_csv:
 		writer = csv.writer(fichier_csv, delimiter=',')
 		writer.writerow(en_tete)
 		for data in datas:
 			writer.writerow(data)
-
 	pass
 	
 def rate(soup):
 	
-	# on cherche uniquement la div qui inclut la note
+# on cherche uniquement la div qui inclut la note
 	div_rating = soup.find_all('div', class_='col-sm-6 product_main')
-	
 	# puis la liste des <P> de cette div
 	p_rating = div_rating[0].find_all('p')
-
-	#on cherche la classe qui indique la note
+# on cherche la classe qui indique la note
 	i=0
-	star = [['star-rating', 'Five'],['star-rating Four'],['star-rating Three'],['star-rating Two'],['star-rating One']]
+	star = [['star-rating', 'Five'],['star-rating', 'Four'],['star-rating', 'Three'],['star-rating', 'Two'],['star-rating', 'One']]
 	note = ['5','4','3','2','1']
 	while i < len(p_rating):
 		tag = p_rating[i]
@@ -55,45 +57,38 @@ def rate(soup):
 			rating = note[4]
 			i = len	(p_rating)
 		else:
-			rating = "pas de note"
-		
+			rating = "pas de note"	
 		i = i + 1
 		pass
-
 	return rating
 
 def listing_url_page_produit(url_section):
 
 	url_section_liste = []
 	links_produit = []
+# on ajoute par defaut la premiere page de la section "index.html"
 	url_section_liste.append(url_section)
-
-	reponse = requests.get(url_section)
-	page_produit = reponse.content
-	soup = BeautifulSoup(page_produit, 'html.parser')
-	
-	# vérification de l'existence d'autre page de la même section
+	soup = parse(url_section)	
+# vérification de l'existence d'autre page de la même section
 	other_pages = soup.find('li', class_="current")
 	if not other_pages:
 		pass
 	else:
-		#on determine combien il y a de page
+# on determine combien il y a de page
 		other_page = other_pages.text.strip()
 		nombre_page = other_page[len(other_page)-1:]
 		i = int(nombre_page)
 		while i > 1:
-			# on ajoute les différentes page dans une liste
+# on ajoute les différentes page dans une liste
 			url_section_liste.append(url_section.replace('index', 'page-' + str(i)))
 			i = i - 1
 
 	for x in url_section_liste:
-		reponse = requests.get(x)
-		page_produit = reponse.content
-		soup = BeautifulSoup(page_produit, 'html.parser')
-		#on va recuperer tout les h3 qui contiennent les url des pages produit
+		soup = parse(x)
+# on va recuperer tout les h3 qui contiennent les url des pages produit
 		url_produits = soup.find_all('h3')	
 		i = 0
-		#dans chaque element trouvé, on recupere l'url href dans la balise <a>
+# dans chaque element trouvé, on recupere l'url href dans la balise <a>
 		for url_produit in url_produits:
 			url_produit = url_produits[i].a
 			links_produit.append("http://books.toscrape.com/catalogue/" + url_produit.get('href').replace('../', ''))
@@ -103,13 +98,9 @@ def listing_url_page_produit(url_section):
 
 def recup_data_produit(url):
 
-	reponse = requests.get(url)
-	page = reponse.content
-
-	#parse de la page en soup
-	soup = BeautifulSoup(page, "html.parser")
+	soup = parse(url)
 	
-	#vérification multiple pour recupérer les données demandées
+# vérification multiple pour recupérer les données demandées
 	i=0
 	excract = soup.find_all("tr")
 	for x in excract:
@@ -127,38 +118,48 @@ def recup_data_produit(url):
 			none=y
 		i=i+1
 
-	# utilisation de la function rate pour recupérer la note
+# utilisation de la function rate pour recupérer la note
 	review_rating = rate(soup)
-	# recuperation du premier H1 qui correspond au titre
+# recuperation du premier H1 qui correspond au titre
 	titre = soup.select('h1')[0].text
-	# recuperation de la description
+# recuperation de la description
 	description = soup.select('p')[3].text
-	# récupération de la categorie en 4eme lien
+# récupération de la categorie en 4eme lien
 	categorie = soup.select('a')[3].text
-	# recupération de l'url de l'image si src = titre
+# recupération de l'url de l'image si src = titre
 	image_urls = soup.find_all('img', alt=titre)
 	links = []
 	for image_url in image_urls:
 		links.append("http://books.toscrape.com/" + image_url['src'].replace('../', ''))
 	image_url = links[0]
-	
 	data = [url, upc, titre, price_incl, price_excl, number_available, description, categorie, review_rating, image_url]
 	
 	return data
 
 def etl():
-	#page à recuperer
-	url_site = "http://books.toscrape.com/index.html"
 
-	url_sections = "http://books.toscrape.com/catalogue/category/books/classics_6/index.html"
-	url_produits = listing_url_page_produit(url_sections)
+# page à recuperer
+	soup = parse("http://books.toscrape.com/index.html")
 
-	datas = []
-	for url_produit in url_produits:
-		datas.append(recup_data_produit(url_produit))
-	
-	#fonction ecriture pour enregistrer les datas dans un csv
-	ecriture(datas, 'data.csv')
-	
+# on vérifie et recupere les URLs et les titres d'une section
+	url_sections = []
+	i = 0
+	for url_section in soup.find_all('a'):
+		if 'catalogue/category/books/' in url_section.get('href'):
+			url_sections.append(url_section.get('href'))
+			titre_section = url_section.get_text()
 
+# puis les URLs des produit
+			url_produits = listing_url_page_produit("http://books.toscrape.com/" + url_sections[i])
+			datas = []
+
+# on recupere les donnees de tout les produits
+			for url_produit in url_produits:
+				datas.append(recup_data_produit(url_produit))
+
+# enfin on enregistre les datas dans un csv au nom de la section
+			ecriture(datas, 'data_'+ titre_section.strip() +'.csv')
+			i = i +1
+		else:
+			pass
 etl()
